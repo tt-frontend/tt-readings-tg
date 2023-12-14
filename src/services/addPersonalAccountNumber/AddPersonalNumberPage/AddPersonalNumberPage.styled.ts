da25c9a354a27a18181ac542ca27dfd596c1dc36
anotherIlnaz <mmmr.sh.i@gmail.com>
2023-12-14 14:20:33 +0300

    [RE6-593]: centered city inputt

--- src/services/addPersonalAccountNumber/AddPersonalNumberPage/AddPersonalNumberPage.styled.ts
+++ src/services/addPersonalAccountNumber/AddPersonalNumberPage/AddPersonalNumberPage.styled.ts
@@ -1,41 +1,45 @@
+import { Select } from "antd";
 import { styled } from "styled-components";
 
 export const PageWrapper = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  height: 90vh;
+   display: flex;
+   align-items: center;
+   justify-content: center;
+   height: 90vh;
 `;
 
 export const WindowWrapper = styled.div`
-  width: 100%;
-  background-color: rgba(24, 158, 233, 0.16);
-  border-radius: 16px;
-  padding: 16px;
-  display: flex;
-  flex-direction: column;
-  gap: 16px;
+   width: 100%;
+   background-color: rgba(24, 158, 233, 0.16);
+   border-radius: 16px;
+   padding: 16px;
+   display: flex;
+   flex-direction: column;
+   gap: 16px;
 `;
 
 export const Title = styled.div`
-  color: #000;
-  font-size: 20px;
-  font-style: normal;
-  font-weight: 600;
-  line-height: 24px;
+   color: #000;
+   font-size: 20px;
+   font-style: normal;
+   font-weight: 600;
+   line-height: 24px;
 `;
 
 export const ButtonsWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 8px;
+   display: flex;
+   flex-direction: column;
+   gap: 8px;
 `;
 
 export const Description = styled.div`
-  color: #000;
-  font-size: 15px;
-  font-style: normal;
-  font-weight: 400;
-  line-height: 20px;
-  margin-top: 4px;
+   color: #000;
+   font-size: 15px;
+   font-style: normal;
+   font-weight: 400;
+   line-height: 20px;
+   margin-top: 4px;
+`;
+
+export const SelectSc = styled(Select)`
 `;
--- src/services/addPersonalAccountNumber/AddPersonalNumberPage/AddPersonalNumberPage.tsx
+++ src/services/addPersonalAccountNumber/AddPersonalNumberPage/AddPersonalNumberPage.tsx
@@ -1,10 +1,11 @@
 import { FC, useCallback, useEffect, useState } from "react";
 import {
-  ButtonsWrapper,
-  Description,
-  PageWrapper,
-  Title,
-  WindowWrapper,
+   ButtonsWrapper,
+   Description,
+   PageWrapper,
+   SelectSc,
+   Title,
+   WindowWrapper,
 } from "./AddPersonalNumberPage.styled";
 import { Select } from "@/components/Select";
 import { Button } from "@/components/Button";
@@ -17,120 +18,126 @@ import { useFormik } from "formik";
 import { getAddressString } from "@/utils/getAddressString";
 
 export const AddPersonalNumberPage: FC<AddPersonalNumberPageProps> = ({
-  existingCities,
-  handleFindAccount,
-  homeownerAccount,
-  isLoading,
-  handleLinkAccount,
+   existingCities,
+   handleFindAccount,
+   homeownerAccount,
+   isLoading,
+   handleLinkAccount,
 }) => {
-  const [currentTab, setCurrentTab] = useState(0);
+   const [currentTab, setCurrentTab] = useState(0);
 
-  const { values, setFieldValue, handleChange, handleSubmit } = useFormik({
-    initialValues: {
-      city: null as null | string,
-      accountNumber: "",
-      apartmentNumber: "",
-    },
-    onSubmit: (values) => {
-      if (!values.city) return;
+   const { values, setFieldValue, handleChange, handleSubmit } = useFormik({
+      initialValues: {
+         city: null as null | string,
+         accountNumber: "",
+         apartmentNumber: "",
+      },
+      onSubmit: (values) => {
+         if (!values.city) return;
 
-      handleFindAccount({
-        City: values.city,
-        AccountNumber: values.accountNumber,
-        ApartmentNumber: values.apartmentNumber,
-      });
-    },
-  });
+         handleFindAccount({
+            City: values.city,
+            AccountNumber: values.accountNumber,
+            ApartmentNumber: values.apartmentNumber,
+         });
+      },
+   });
 
-  const next = useCallback(() => {
-    setCurrentTab((prev) => prev + 1);
-  }, [setCurrentTab]);
+   const next = useCallback(() => {
+      setCurrentTab((prev) => prev + 1);
+   }, [setCurrentTab]);
 
-  const back = useCallback(() => {
-    setCurrentTab((prev) => prev - 1);
-  }, [setCurrentTab]);
+   const back = useCallback(() => {
+      setCurrentTab((prev) => prev - 1);
+   }, [setCurrentTab]);
 
-  useEffect(() => {
-    if (homeownerAccount) setCurrentTab(2);
-  }, [homeownerAccount]);
+   useEffect(() => {
+      if (homeownerAccount) setCurrentTab(2);
+   }, [homeownerAccount]);
 
-  return (
-    <PageWrapper>
-      {currentTab === 0 && (
-        <WindowWrapper>
-          <Title>Выберите город</Title>
-          <Select
-            value={values.city}
-            placeholder="город"
-            onChange={(value) => setFieldValue("city", value)}
-            allowClear
-          >
-            {existingCities?.map((city) => (
-              <Select.Option value={city}>{city}</Select.Option>
-            ))}
-          </Select>
-          <Button type="primary" onClick={next} disabled={!values.city}>
-            Продолжить
-          </Button>
-        </WindowWrapper>
-      )}
-      {currentTab === 1 && (
-        <WindowWrapper>
-          <Title>Введите данные квартиры</Title>
-          <FormItem label="Номер лицевого счета">
-            <Input
-              placeholder="000000000000"
-              value={values.accountNumber}
-              onChange={handleChange}
-              name="accountNumber"
-            />
-          </FormItem>
-          <FormItem label="Номер квартиры">
-            <Input
-              value={values.apartmentNumber}
-              onChange={handleChange}
-              name="apartmentNumber"
-            />
-          </FormItem>
-          <ButtonsWrapper>
-            <Button
-              type="primary"
-              onClick={() => handleSubmit()}
-              disabled={
-                !values.accountNumber || !values.apartmentNumber || isLoading
-              }
-            >
-              Продолжить
-            </Button>
-            <Button type="primary" ghost onClick={back}>
-              Вернуться назад
-            </Button>
-          </ButtonsWrapper>
-        </WindowWrapper>
-      )}
-      {currentTab === 2 && homeownerAccount && (
-        <WindowWrapper>
-          <div>
-            <Title>Мы нашли адрес!</Title>
-            <Description>Пожалуйста, проверьте адрес квартиры.</Description>
-          </div>
-          <LinkInfoPanel
-            icon={<Building />}
-            title={getAddressString(homeownerAccount.address)}
-          />
-          <ButtonsWrapper>
-            <Button
-              type="primary"
-              onClick={() => handleLinkAccount(homeownerAccount.accountId)}
-            >
-              Все верно
-            </Button>
-            <Button type="primary" ghost onClick={back}>
-              Вернуться назад
-            </Button>
-          </ButtonsWrapper>
-        </WindowWrapper>
-      )}
-    </PageWrapper>
-  );
+   return (
+      <PageWrapper>
+         {currentTab === 0 && (
+            <WindowWrapper>
+               <Title>Выберите город</Title>
+               <Select
+                  value={values.city}
+                  placeholder="город"
+                  onChange={(value) => setFieldValue("city", value)}
+                  allowClear
+               >
+                  {existingCities?.map((city) => (
+                     <Select.Option value={city}>{city}</Select.Option>
+                  ))}
+               </Select>
+               <Button type="primary" onClick={next} disabled={!values.city}>
+                  Продолжить
+               </Button>
+            </WindowWrapper>
+         )}
+         {currentTab === 1 && (
+            <WindowWrapper>
+               <Title>Введите данные квартиры</Title>
+               <FormItem label="Номер лицевого счета">
+                  <Input
+                     placeholder="000000000000"
+                     value={values.accountNumber}
+                     onChange={handleChange}
+                     name="accountNumber"
+                  />
+               </FormItem>
+               <FormItem label="Номер квартиры">
+                  <Input
+                     value={values.apartmentNumber}
+                     onChange={handleChange}
+                     name="apartmentNumber"
+                  />
+               </FormItem>
+               <ButtonsWrapper>
+                  <Button
+                     type="primary"
+                     onClick={() => handleSubmit()}
+                     disabled={
+                        !values.accountNumber ||
+                        !values.apartmentNumber ||
+                        isLoading
+                     }
+                  >
+                     Продолжить
+                  </Button>
+                  <Button type="primary" ghost onClick={back}>
+                     Вернуться назад
+                  </Button>
+               </ButtonsWrapper>
+            </WindowWrapper>
+         )}
+         {currentTab === 2 && homeownerAccount && (
+            <WindowWrapper>
+               <div>
+                  <Title>Мы нашли адрес!</Title>
+                  <Description>
+                     Пожалуйста, проверьте адрес квартиры.
+                  </Description>
+               </div>
+               <LinkInfoPanel
+                  icon={<Building />}
+                  title={getAddressString(homeownerAccount.address)}
+               />
+               <ButtonsWrapper>
+                  <Button
+                     type="primary"
+                     onClick={() =>
+                        handleLinkAccount(homeownerAccount.accountId)
+                     }
+                  >
+                     Все верно
+                  </Button>
+                  <Button type="primary" ghost onClick={back}>
+                     Вернуться назад
+                  </Button>
+               </ButtonsWrapper>
+            </WindowWrapper>
+         )}
+      </PageWrapper>
+   );
 };
