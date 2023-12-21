cb36ccf345d747a7f6948d73b4405ad90840adad
anotherIlnaz <mmmr.sh.i@gmail.com>
2023-12-21 14:25:32 +0300

    [RE6-607]

--- src/services/mainPage/MainPage/MainPage.tsx
+++ src/services/mainPage/MainPage/MainPage.tsx
@@ -2,10 +2,10 @@ import { LinkInfoPanel } from "@/components/LinkInfoPanel";
 import { Building } from "@/components/icons/Building";
 import { Bag } from "@/components/icons/Bag";
 import {
-  ActionsTitle,
-  ActionsWrapper,
-  InfoLinksWrapper,
-  Wrapper,
+   ActionsTitle,
+   ActionsWrapper,
+   InfoLinksWrapper,
+   Wrapper,
 } from "./MainPage.styled";
 import { PersonalNumbersPanel } from "./PersonalNumbersPanel/PersonalNumbersPanel";
 import { ActionLink } from "@/components/ActionLink";
@@ -15,50 +15,51 @@ import { getAddressString } from "@/utils/getAddressString";
 import { Skeleton } from "antd";
 
 export const MainPage: FC<MainPageProps> = ({
-  selectedPersonalNumber,
-  setSelectedPersonalNumber,
-  homeownerAccounts,
-  currentHomeownerAccount,
-  isLoadingHomeownerAccount,
+   selectedPersonalNumber,
+   setSelectedPersonalNumber,
+   homeownerAccounts,
+   currentHomeownerAccount,
+   isLoadingHomeownerAccount,
 }) => {
-  return (
-    <Wrapper>
-      <PersonalNumbersPanel
-        selectedNumber={selectedPersonalNumber}
-        handleSelect={setSelectedPersonalNumber}
-        personalNumbers={homeownerAccounts || []}
-      />
-      {isLoadingHomeownerAccount && <Skeleton active />}
-      {currentHomeownerAccount && !isLoadingHomeownerAccount && (
-        <>
-          <InfoLinksWrapper>
-            <LinkInfoPanel
-              icon={<Building />}
-              title={getAddressString(currentHomeownerAccount.address)}
-            />
-            <LinkInfoPanel
-              icon={<Bag />}
-              title={
-                currentHomeownerAccount.managementFirmTitle || "Нет данных"
-              }
-            />
-          </InfoLinksWrapper>
-          <ActionsTitle>Что вы хотите сделать?</ActionsTitle>
-          <ActionsWrapper>
-            <ActionLink
-              title="Ввести показания счетчиков"
-              path="/inputReadings"
-            />
-            {/* <ActionLink title="Отправить заявку на опломбировку" path="" />
+   return (
+      <Wrapper>
+         <PersonalNumbersPanel
+            selectedNumber={selectedPersonalNumber}
+            handleSelect={setSelectedPersonalNumber}
+            personalNumbers={homeownerAccounts || []}
+         />
+         {isLoadingHomeownerAccount && <Skeleton active />}
+         {currentHomeownerAccount && !isLoadingHomeownerAccount && (
+            <>
+               <InfoLinksWrapper>
+                  <LinkInfoPanel
+                     icon={<Building />}
+                     title={getAddressString(currentHomeownerAccount.address)}
+                  />
+                  <LinkInfoPanel
+                     icon={<Bag />}
+                     title={
+                        currentHomeownerAccount.managementFirmTitle ||
+                        "Нет данных"
+                     }
+                  />
+               </InfoLinksWrapper>
+               <ActionsTitle>Что вы хотите сделать?</ActionsTitle>
+               <ActionsWrapper>
+                  <ActionLink
+                     title="Ввести показания счетчиков"
+                     path="/inputReadings"
+                  />
+                  {/* <ActionLink title="Отправить заявку на опломбировку" path="" />
             <ActionLink title="Отправить заявку на проверку" path="" />
             <ActionLink
               title="Заявить о проблеме"
               description="Если прорвало трубу, перегорела лампочка или нет электричества"
               path=""
             /> */}
-          </ActionsWrapper>
-        </>
-      )}
-    </Wrapper>
-  );
+               </ActionsWrapper>
+            </>
+         )}
+      </Wrapper>
+   );
 };
