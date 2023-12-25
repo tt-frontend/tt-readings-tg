7b2fce3f71ed16a9a27c84f60b68ca81d62e0891
anotherIlnaz <mmmr.sh.i@gmail.com>
2023-12-25 21:49:55 +0300

    [RE6-607]

--- src/services/mainPage/MainPage/MainPage.tsx
+++ src/services/mainPage/MainPage/MainPage.tsx
@@ -22,59 +22,59 @@ export const MainPage: FC<MainPageProps> = ({
    currentHomeownerAccount,
    isLoadingHomeownerAccount,
 }) => {
-
-  return (
-    <Wrapper>
-      <PersonalNumbersPanel
-        selectedNumber={selectedPersonalNumber}
-        handleSelect={setSelectedPersonalNumber}
-        personalNumbers={homeownerAccounts || []}
-      />
-      {currentHomeownerAccount && (
-        <>
-          <InfoLinksWrapper>
-            <LinkInfoPanel
-              icon={<Building />}
-              title={getAddressString(currentHomeownerAccount.address)}
-               loader={{
+   return (
+      <Wrapper>
+         <PersonalNumbersPanel
+            selectedNumber={selectedPersonalNumber}
+            handleSelect={setSelectedPersonalNumber}
+            personalNumbers={homeownerAccounts || []}
+         />
+         {currentHomeownerAccount && (
+            <>
+               <InfoLinksWrapper>
+                  <LinkInfoPanel
+                     icon={<Building />}
+                     title={getAddressString(currentHomeownerAccount.address)}
+                     loader={{
                         state: isLoadingHomeownerAccount,
                         view: <LongSkeleton active />,
                      }}
-            />
-            <LinkInfoPanel
-              icon={<Bag />}
-              title={
-                currentHomeownerAccount.managementFirmTitle || "Нет данных"
-              }
-              link="/managementFirm"
-              loader={{
+                  />
+                  <LinkInfoPanel
+                     icon={<Bag />}
+                     title={
+                        currentHomeownerAccount.managementFirmTitle ||
+                        "Нет данных"
+                     }
+                     link="/managementFirm"
+                     loader={{
                         state: isLoadingHomeownerAccount,
                         view: <Skeleton.Input active />,
                      }}
-            />
-          </InfoLinksWrapper>
-          <ActionsTitle>Что вы хотите сделать?</ActionsTitle>
-          <ActionsWrapper>
-            <ActionLink
-              title="Ввести показания счетчиков"
-              path="/inputReadings"
-            />
-            <ActionLink
-              title="Отправить заявку на опломбировку"
-              path="/managementFirm"
-            />
-            <ActionLink
-              title="Отправить заявку на проверку"
-              path="/managementFirm"
-            />
-            <ActionLink
-              title="Заявить о проблеме"
-              description="Если прорвало трубу, перегорела лампочка или нет электричества"
-              path="/managementFirm"
-            />
-          </ActionsWrapper>
-        </>
-      )}
-    </Wrapper>
-  );
+                  />
+               </InfoLinksWrapper>
+               <ActionsTitle>Что вы хотите сделать?</ActionsTitle>
+               <ActionsWrapper>
+                  <ActionLink
+                     title="Ввести показания счетчиков"
+                     path="/inputReadings"
+                  />
+                  <ActionLink
+                     title="Отправить заявку на опломбировку"
+                     path="/managementFirm"
+                  />
+                  <ActionLink
+                     title="Отправить заявку на проверку"
+                     path="/managementFirm"
+                  />
+                  <ActionLink
+                     title="Заявить о проблеме"
+                     description="Если прорвало трубу, перегорела лампочка или нет электричества"
+                     path="/managementFirm"
+                  />
+               </ActionsWrapper>
+            </>
+         )}
+      </Wrapper>
+   );
 };
--- src/services/mainPage/MainPage/PersonalNumbersPanel/PersonalNumbersPanel.tsx
+++ src/services/mainPage/MainPage/PersonalNumbersPanel/PersonalNumbersPanel.tsx
@@ -1,43 +1,90 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 import {
-  AddPersonalNumberButton,
-  CheckCircle,
-  PersonalNumber,
-  Wrapper,
+   AddPersonalNumberButton,
+   CheckCircle,
+   PersonalNumber,
+   Wrapper,
 } from "./PersonalNumbersPanel.styled";
 import { PersonalNumbersPanelProps } from "./PersonalNumbersPanel.types";
 import { Check } from "@/components/icons/Check";
 import { Plus } from "@/components/icons/Plus";
 import { useNavigate } from "react-router-dom";
+import { Popconfirm } from "antd";
 
 export const PersonalNumbersPanel: FC<PersonalNumbersPanelProps> = ({
-  personalNumbers,
-  selectedNumber,
-  handleSelect,
+   personalNumbers,
+   selectedNumber,
+   handleSelect,
 }) => {
-  const navigate = useNavigate();
+   const navigate = useNavigate();
 
-  return (
-    <Wrapper>
-      {personalNumbers.map((elem) => (
-        <PersonalNumber
-          onClick={() => handleSelect(elem.accountId)}
-          key={elem.accountId}
-          isActive={elem.accountId === selectedNumber}
-        >
-          {elem.accountNumber}
-          {elem.accountId === selectedNumber && (
-            <CheckCircle>
-              <Check />
-            </CheckCircle>
-          )}
-        </PersonalNumber>
-      ))}
-      <AddPersonalNumberButton
-        onClick={() => navigate("/addPersonalAccountNumber")}
-      >
-        <Plus /> Добавить лицевой счет
-      </AddPersonalNumberButton>
-    </Wrapper>
-  );
+   const [onMouseDownTime, setOnMouseDownTime] = useState<number | null>(null);
+
+   // let longPressTimer: NodeJS.Timeout | null = null;
+
+   const [longPressTimer, setLongPressTimer] = useState<NodeJS.Timeout | null>(
+      null
+   );
+
+   const [open, setOpen] = useState<boolean>(false);
+
+   return (
+      <Wrapper>
+         {personalNumbers.map((elem) => (
+            <Popconfirm
+               key={elem.accountId}
+               title="Вы хотите удалить лицевой счёт?"
+               // description="ВЫ хотите удалить лицевой счёт"
+               open={open}
+               onConfirm={() => {
+                  setOpen(false);
+                  console.log({ accountId: elem.accountId });
+               }}
+               // okButtonProps={{ loading: confirmLoading }}
+               onCancel={() => setOpen(false)}
+            >
+               <PersonalNumber
+                  key={elem.accountId}
+                  isActive={elem.accountId === selectedNumber}
+                  onMouseDown={(event) => {
+                     const startTime = event.timeStamp;
+                     setOnMouseDownTime(event.timeStamp);
+
+                     const timeout = 1000;
+
+                     setLongPressTimer(
+                        setTimeout(() => {
+                           if (Date.now() - startTime > timeout) {
+                              console.log("long");
+                              setOpen(true);
+                              return;
+                           }
+                        }, timeout)
+                     );
+                  }}
+                  onMouseUp={(event) => {
+                     longPressTimer && clearTimeout(longPressTimer);
+
+                     if (event.timeStamp - onMouseDownTime! < 1000) {
+                        console.log("single shot");
+                        handleSelect(elem.accountId);
+                     }
+                  }}
+               >
+                  {elem.accountNumber}
+                  {elem.accountId === selectedNumber && (
+                     <CheckCircle>
+                        <Check />
+                     </CheckCircle>
+                  )}
+               </PersonalNumber>
+            </Popconfirm>
+         ))}
+         <AddPersonalNumberButton
+            onClick={() => navigate("/addPersonalAccountNumber")}
+         >
+            <Plus /> Добавить лицевой счет
+         </AddPersonalNumberButton>
+      </Wrapper>
+   );
 };
