32b9da6fe267fcb92226bd5b3d855bb47e8a7633
Iskander Karimov <41307855+pronix575@users.noreply.github.com>
2024-01-16 09:01:06 +0300

    [RE6-611]: Implements readings input

--- src/services/inputReadings/InputReadingsPage/DeviceReadingInput/ReadingInput/ReadingInput.tsx
+++ src/services/inputReadings/InputReadingsPage/DeviceReadingInput/ReadingInput/ReadingInput.tsx
@@ -1,12 +1,12 @@
-import { FC } from "react";
+import { FC, useEffect, useState } from "react";
 import {
-   DeviceReadingsInfoWrapper,
-   ErrorMessage,
-   Input,
-   LastReading,
-   ReadingMonth,
-   ReadingsConsumption,
-   Wrapper,
+  DeviceReadingsInfoWrapper,
+  ErrorMessage,
+  Input,
+  LastReading,
+  ReadingMonth,
+  ReadingsConsumption,
+  Wrapper,
 } from "./ReadingInput.styled";
 import { Props } from "./ReadingInput.types";
 import dayjs from "dayjs";
@@ -14,60 +14,67 @@ import { round } from "lodash";
 import { useSwitchInputOnEnter } from "@/utils/useSwitchInputOnEnter";
 
 export const ReadingInput: FC<Props> = ({
-   value,
-   handleChange,
-   placeholder,
-   prevReadingDate,
-   prevReadingValue,
-   validationResult,
-   unit,
-   inputNumber,
+  value,
+  handleChange,
+  placeholder,
+  prevReadingDate,
+  prevReadingValue,
+  validationResult,
+  unit,
+  inputNumber,
 }) => {
-   const next = useSwitchInputOnEnter("bot-readings", false);
+  const [innerValue, setInnerValue] = useState("");
 
-   const consumption = (value || 0) - Number(prevReadingValue);
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const inputValue = event.target.value;
+    if (/^\d*[\\.,]?\d{0,3}$/.test(inputValue)) {
+      setInnerValue(inputValue);
+    }
+  };
 
-   const consumptionString = round(consumption, 3);
+  useEffect(() => {
+    handleChange(innerValue ? Number(innerValue) : null);
+  }, [innerValue]);
 
-   return (
-      <Wrapper>
-         <Input
-            // type="number"
-            error={validationResult?.type}
-            value={value === null ? "" : String(value)}
-            onChange={(e) => {
-               const value = Number(e.target.value);
+  const next = useSwitchInputOnEnter("bot-readings", false);
 
-               if (Number.isNaN(value)) return;
+  const consumption = (value || 0) - Number(prevReadingValue);
 
-               handleChange(e.target.value === "" ? null : value);
-            }}
-            placeholder={placeholder}
-            onKeyDown={(key) => {
-               if (key.code === "Enter") {
-                  next(inputNumber);
-               }
-            }}
-            data-reading-input="bot-readings"
-         />
-         {validationResult && (
-            <ErrorMessage errorType={validationResult.type}>
-               {validationResult.message}
-            </ErrorMessage>
-         )}
-         <DeviceReadingsInfoWrapper>
-            <LastReading>
-               <ReadingMonth>
-                  {dayjs(prevReadingDate).format("MMMM YYYY")}:
-               </ReadingMonth>
-               {prevReadingValue} {unit}
-            </LastReading>
-            {Boolean(value) && prevReadingValue && (
-               <ReadingsConsumption>
-                  Расход: {consumptionString} {unit}
-               </ReadingsConsumption>
-            )}
-         </DeviceReadingsInfoWrapper>
-      </Wrapper>
-   );
+  const consumptionString = round(consumption, 3);
+
+  return (
+    <Wrapper>
+      <Input
+        type="text"
+        error={validationResult?.type}
+        value={innerValue}
+        onChange={handleInputChange}
+        placeholder={placeholder}
+        onKeyDown={(key) => {
+          if (key.code === "Enter") {
+            next(inputNumber);
+          }
+        }}
+        data-reading-input="bot-readings"
+      />
+      {validationResult && (
+        <ErrorMessage errorType={validationResult.type}>
+          {validationResult.message}
+        </ErrorMessage>
+      )}
+      <DeviceReadingsInfoWrapper>
+        <LastReading>
+          <ReadingMonth>
+            {dayjs(prevReadingDate).format("MMMM YYYY")}:
+          </ReadingMonth>
+          {prevReadingValue} {unit}
+        </LastReading>
+        {Boolean(value) && prevReadingValue && (
+          <ReadingsConsumption>
+            Расход: {consumptionString} {unit}
+          </ReadingsConsumption>
+        )}
+      </DeviceReadingsInfoWrapper>
+    </Wrapper>
+  );
 };
