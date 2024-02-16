dca6f3828a54526321ecf73649a4fd8e7456f57d
Iskander Karimov <41307855+pronix575@users.noreply.github.com>
2024-02-16 14:27:18 +0300

    [TT-109]: Add defult token

--- src/Router/Router.tsx
+++ src/Router/Router.tsx
@@ -4,19 +4,29 @@ import { getRoutes } from "./Routes.constants";
 import { useUnit } from "effector-react";
 import { authService } from "@/services/authService";
 import { useBackButton } from "@/services/backButton/backButtonService.hook";
+import { DEFAULT_TOKEN } from "@/services/authService/authService.model";
 
 export const Router = () => {
   useBackButton();
 
-  const isAuth = useUnit(authService.outputs.$isAuth);
+  const { isAuth, setAuthToken } = useUnit({
+    isAuth: authService.outputs.$isAuth,
+    setAuthToken: authService.inputs.setAuthToken,
+  });
 
   const navigate = useNavigate();
 
   const [params] = useSearchParams();
 
   useEffect(() => {
+    if (DEFAULT_TOKEN) {
+      setAuthToken(DEFAULT_TOKEN);
+
+      return;
+    }
+
     if (!isAuth) navigate(`/limb`);
-  }, [navigate, params, isAuth]);
+  }, [navigate, params, isAuth, setAuthToken]);
 
   const routes = useMemo(() => getRoutes(), []);
 
--- src/services/authService/authService.model.ts
+++ src/services/authService/authService.model.ts
@@ -9,12 +9,16 @@ const fetchAuthTokenFx = createEffect<string, LoginResponse, AxiosError>(
   loginUser
 );
 
-const $authToken = createStore<null | string>(null).on(
-  fetchAuthTokenFx.doneData,
-  (_, data) => {
+const setAuthToken = createEvent<string>();
+
+export const DEFAULT_TOKEN =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJUZ1VzZXJJZCI6IjE3NjcyNDIwNzMiLCJUZ1VzZXJJZEludGVybmFsIjoiMGYzNmY5ZjctNzlmOC00ZTRiLTlhYzQtMThmNGI1MGIxMjYzIiwiVGdBdXRoRmxhZyI6InRydWUiLCJuYmYiOjE3MDgwNzc2ODcsImV4cCI6MTcwODA5MjA4NywiaWF0IjoxNzA4MDc3Njg3fQ.0dEVPRmyZjDrmRTr7EHILxi_BsDJ_IQL-b3xyqscR2Y";
+
+const $authToken = createStore<null | string>(null)
+  .on(fetchAuthTokenFx.doneData, (_, data) => {
     return data.token;
-  }
-);
+  })
+  .on(setAuthToken, (_, token) => token);
 
 sample({
   source: $authToken,
@@ -34,6 +38,6 @@ fetchAuthTokenFx.failData.watch((e) => {
 const $isAuth = $authToken.map(Boolean);
 
 export const authService = {
-  inputs: { handleSecretRecieved },
+  inputs: { handleSecretRecieved, setAuthToken },
   outputs: { $authToken, $isAuth },
 };
