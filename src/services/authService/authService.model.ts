6b75b83ffbe04d776092be1478baa8c585595364
Iskander Karimov <41307855+pronix575@users.noreply.github.com>
2025-03-05 15:25:59 +0300

    fix

--- src/services/authService/authService.model.ts
+++ src/services/authService/authService.model.ts
@@ -11,8 +11,7 @@ const fetchAuthTokenFx = createEffect<string, LoginResponse, AxiosError>(
 
 const setAuthToken = createEvent<string>();
 
-export const DEFAULT_TOKEN =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJUZ1VzZXJJZCI6IjE3NjcyNDIwNzMiLCJUZ1VzZXJJZEludGVybmFsIjoiMGYzNmY5ZjctNzlmOC00ZTRiLTlhYzQtMThmNGI1MGIxMjYzIiwiVGdBdXRoRmxhZyI6InRydWUiLCJuYmYiOjE3NDExMDc3MDYsImV4cCI6MTc0MTEyMjEwNiwiaWF0IjoxNzQxMTA3NzA2fQ.EQ90vz7YffxXcbbnDfkgcEp1YALcPHEbjRqgqIu2DDc";
+export const DEFAULT_TOKEN = null;
 
 const $authToken = createStore<null | string>(null)
   .on(fetchAuthTokenFx.doneData, (_, data) => {
